08ae344ec08e1d2405676bf6c2337fef361c6595
Kazuha <devsinghharsh000@gmail.com>
2025-02-25 12:13:03 +0530

    Update SwapService.js

--- src/services/SwapService.js
+++ src/services/SwapService.js
@@ -1,49 +1,88 @@
 const { ethers } = require("ethers");
 const BaseService = require("./BaseService");
-const config = require("../config/config.json");
+const WMonContract = require("../contracts/WMonContract");
+const Utils = require("../core/Utils");
+const config = require("../../config/config");
 
 class SwapService extends BaseService {
   constructor() {
     super();
-    this.wmonContract = new ethers.Contract(
-      config.contracts.wmon,
-      [
-        "function deposit() public payable",
-        "function withdraw(uint256 amount) public",
-      ],
-      this.wallet
-    );
+    this.wmonContract = new WMonContract();
   }
 
   async wrapMON(amount) {
     try {
-      const tx = await this.wmonContract.deposit({
-        value: amount,
-        gasLimit: BigInt(config.gas.stake),
-      });
+      await this.checkGasPrice();
 
-      const receipt = await tx.wait();
+      Utils.logger(
+        "info",
+        `Wrapping ${Utils.formatAmount(amount)} MON to WMON`
+      );
+      const receipt = await this.wmonContract.deposit(amount);
 
       return {
         status: receipt.status === 1 ? "Success" : "Failed",
+        txHash: receipt.hash,
+        url: Utils.logTransaction(receipt.hash),
       };
     } catch (error) {
+      Utils.logger("error", `Error wrapping MON: ${error.message}`);
       throw error;
     }
   }
 
   async unwrapMON(amount) {
     try {
-      const tx = await this.wmonContract.withdraw(amount, {
-        gasLimit: BigInt(config.gas.stake),
-      });
+      await this.checkGasPrice();
 
-      const receipt = await tx.wait();
+      Utils.logger(
+        "info",
+        `Unwrapping ${Utils.formatAmount(amount)} WMON to MON`
+      );
+      const receipt = await this.wmonContract.withdraw(amount);
 
       return {
         status: receipt.status === 1 ? "Success" : "Failed",
+        txHash: receipt.hash,
+        url: Utils.logTransaction(receipt.hash),
       };
     } catch (error) {
+      Utils.logger("error", `Error unwrapping WMON: ${error.message}`);
+      throw error;
+    }
+  }
+
+  async getWMonBalance() {
+    try {
+      const walletAddress = this.getWalletAddress();
+      const balance = await this.wmonContract.getBalance(walletAddress);
+      return balance;
+    } catch (error) {
+      Utils.logger("error", `Error getting WMON balance: ${error.message}`);
+      throw error;
+    }
+  }
+
+  async estimateWrapGas(amount) {
+    try {
+      const gasEstimate = await this.wmonContract.estimateGas("deposit", [], {
+        value: amount,
+      });
+      return gasEstimate;
+    } catch (error) {
+      Utils.logger("error", `Error estimating wrap gas: ${error.message}`);
+      throw error;
+    }
+  }
+
+  async estimateUnwrapGas(amount) {
+    try {
+      const gasEstimate = await this.wmonContract.estimateGas("withdraw", [
+        amount,
+      ]);
+      return gasEstimate;
+    } catch (error) {
+      Utils.logger("error", `Error estimating unwrap gas: ${error.message}`);
       throw error;
     }
   }
